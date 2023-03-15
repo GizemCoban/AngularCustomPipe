59383bfd2f869d19c9bfadcf62a9b875c73dc6fc
Gizem Çoban <gizem_coban@outlook.com>
2023-03-15 19:22:32 +0300

    Angular Custom Pipe Example

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TempConvertorPipe } from './temp-convertor.pipe';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TempConvertorPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
